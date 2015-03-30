7aef5542a5958929e365bd5651aedccc389add51
Dilip <dilipmakwana7@yahoo.in>
2015-03-30 10:54:21 +0000

    Starting express boilerplate

---
+++ server.js
@@ -0,0 +1,9 @@
+require('coffee-script/register');
+var express = require('express');
+
+bodyParser = require("body-parser");
+cookieParser = require('cookie-parser');
+session = require('express-session');
+methodOverride = require('method-override');
+errorHandler = require('errorhandler');
+
