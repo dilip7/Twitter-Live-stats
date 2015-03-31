4dd67d0dbe9e0a08c4843b46c9173667a13c4144
Dilip <dilipmakwana7@yahoo.in>
2015-03-31 19:12:47 +0530

    Helmet hooked up

--- server.js
+++ server.js
@@ -6,4 +6,10 @@ cookieParser = require('cookie-parser');
 session = require('express-session');
 methodOverride = require('method-override');
 errorHandler = require('errorhandler');
+helmet = require('helmet');
+csrf = require ('csurf');
 
+var app;
+app = module.exports = express();
+app.set('trust proxy',1);
+app.disable('x-powered-by');
