73acefee3f67b54cbf7ad53864eb248959df7f9e
Dilip <dilipmakwana7@yahoo.in>
2015-04-01 19:04:02 +0530

    Boilerplat v1 final

--- routers.coffee
+++ routers.coffee
@@ -1,9 +1,6 @@
-config = require 'config'
 routes = (app) ->
 
   app.get '/' , (req,res) ->
-    res.render "#{__dirname}/views/qw",
-      token : req.csrfToken()
-      recaptcha_key : "#{config.recaptcha.public_key}"
+    res.send "Shit works!"
 
 module.exports = routes
--- server.js
+++ server.js
@@ -12,11 +12,13 @@ helmet = require('helmet');
 csrf = require ('csurf');
 
 var app;
+
 app = module.exports = express();
 app.set('trust proxy',1);
 app.disable('x-powered-by');
 
 app.use(cookieParser());
+app.set('view engine', 'jade');
 
 app.use(session({
   secret: "asdodweodnqjndoskxmlaskxmklsmx",
@@ -24,3 +26,18 @@ app.use(session({
   resave:true,
   saveUninitialized : true
 }));
+
+app.use(express.static(__dirname + '/assets'));
+
+webappRouter = express.Router();
+webappRouter.use(csrf());
+webappRoutes = require('./routers')(webappRouter);
+app.use('/',webappRoutes);
+
+var server = null;
+server = require('http').createServer(app);
+port = require('./port');
+server.listen(port,function(){
+  console.log("Express boilerplate running on port %d in %s mode", port, app.settings.env);
+  console.log('Server\'s UID is now ' + process.getuid());
+});
